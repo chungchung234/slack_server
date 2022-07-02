ecb0ec36ce976d6ea849f4daae2daa46c6cf59ea
이충현 <a8207637@naver.com>
2022-07-02 15:35:39 +0900

    Initial Commit

---
+++ src/main/java/com/chung/rrs/RrsApplication.java
@@ -0,0 +1,13 @@
+package com.chung.rrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chung/rrs/RrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chung.rrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
